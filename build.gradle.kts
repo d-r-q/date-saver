1d7e21da3eb9b440710fda9d92ce07d93f7b883e
Aleksey Zhidkov <aleksey.zhidkov@pm.me>
2021-05-25 16:24:42 +0700

    Big Ball of Mud

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.5.0"
+    application
+    id("org.openjfx.javafxplugin") version "0.0.10"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.h2database:h2:1.4.200")
+}
+
+application {
+    mainClass.value("pro.azhidkov.solid.DateSaver")
+}
+
+javafx {
+    version = "11.0.2"
+    modules = listOf("javafx.controls")
+}
